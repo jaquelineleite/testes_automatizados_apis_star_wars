0c903ec80debf884e36771dd4edab63ba3f29de3
Jaqueline de Andrade <62401811+jaquelineleite@users.noreply.github.com>
2026-08-14 21:34:01 +0000

    test: atualiza cenarios da SWAPI e dependencias

--- Gemfile
+++ Gemfile
@@ -2,3 +2,10 @@ source 'https://rubygems.org'
 
 gem 'httparty'
 gem 'rspec'
+gem "base64", "~> 0.3.0"
+
+gem "bigdecimal", "~> 4.1"
+
+gem "csv", "~> 3.3"
+
+gem "json-schema", "~> 6.2"
--- spec/swapi_spec.rb
+++ spec/swapi_spec.rb
@@ -1,135 +1,148 @@
 describe '1. Validar o formato da request (json válido) para a seguinte API: https://swapi.dev/api/films/?format=json', :teste_1 do
-    it 'get' do
-        response = SWAPI.get('/api/films/?format=json')
-                
-        # VALIDANDO A RESPOSTA.
-        # ------------------------------------------------------------------------------
-        # Retorna o status code da resposta .code
-        # Retorna todas as informacoes da resposta .inspect
-        # Retorna o valor da requisição de atributo .request
-        # Retorna o valor da resposta do atributo .response
-        # Retorna o valor dos cabeçalhos de atributo .headers
+  it 'get' do
+    response = SWAPI.get('/api/films/?format=json')
 
-        # Retorna o valor do atributo body .body
-        # Exemplo: puts "\nresponse body: #{response.body}"
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+    puts "\nresponse body: #{response.parsed_response}"
 
-        # Retorna a classe .class
-        # Converte para json pq vem como string .parsed_response
-        # Retorna apenas os dados do nome .parsed_response['data']['attributes']['name']
+    expect(response.code).to eq 200
+    expect(response.message).to eq 'OK'
 
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers}"
-        puts "\nresponse body: #{response.parsed_response}"
-
-        # Status HTTP 200 (OK)
-        # O código de status HTTP Status 200 (OK) indica que o request foi processado com sucesso no servidor.
-        # O response payload depende do método HTTP que foi selecionado para o request.
-        expect(response.code).to eq 200
-        expect(response.message).to eq "OK"
-    end
+    # Valida se o retorno é JSON
+    expect(response.headers['content-type'].to_s).to include('application/json')
+    expect(response.parsed_response).to be_a(Hash)
+    expect(response.parsed_response['results']).to be_a(Array)
+  end
 end
+
 
 describe '2. Validar se o retorno HTTP é válido para um GET', :teste_2 do
-    it 'get' do
-        response = SWAPI.get('/api/films/?format=json')
-                
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers['allow']}"
-        
-        expect(response.headers['allow']).to eq "GET, HEAD, OPTIONS"
-    end
+  it 'get' do
+    response = SWAPI.get('/api/films/?format=json')
+
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+
+    expect(response.code).to eq 200
+    expect(response.message).to eq 'OK'
+    expect(response.headers['content-type'].to_s).to include('application/json')
+  end
 end
 
-describe '3. Validar retornos para URLs inválidas, como por exemplo: https://swapi.dev/api/people/?format=jsonx', :teste_3 do
-    it 'get' do
-        response = SWAPI.get('/api/people/?format=jsonx')
-                
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers}"
-        
-        expect(response.code).to eq 200
-        expect(response.message).to eq "OK"
-    end
+
+describe '3. Validar retornos para URLs inválidas', :teste_3 do
+  it 'get' do
+    response = SWAPI.get('/api/people/999999/')
+
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+    puts "\nresponse body: #{response.parsed_response}"
+
+    expect(response.code).to eq 404
+    expect(response.message).to eq 'Not Found'
+    expect(response.headers['content-type'].to_s).to include('application/json')
+  end
 end
 
-describe '4. Validar se o filme 10 é válido e qual o tipo de retorno ao consultar;', :teste_4 do
-    it 'get' do
-        response = SWAPI.get('/api/films/10')
-                
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers}"
-        
-        expect(response.code).to eq 200
-        expect(response.message).to eq "OK"
-    end
+
+describe '4. Validar se o filme 10 é válido e qual o tipo de retorno ao consultar', :teste_4 do
+  it 'get' do
+    response = SWAPI.get('/api/films/10/')
+
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+    puts "\nresponse body: #{response.parsed_response}"
+
+    expect(response.code).to eq 404
+    expect(response.message).to eq 'Not Found'
+    expect(response.headers['content-type'].to_s).to include('application/json')
+    expect(response.parsed_response).to be_a(Hash)
+  end
 end
 
-describe '5. Validar o nome correto de um determinado episódio de filme;', :teste_5 do
-    it 'get' do
-        response = SWAPI.get('/api/films/1')
-                
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers}"
-        puts "\nresponse body: #{response.parsed_response}"
-        
-        expect(response.parsed_response['title']).to eq 'A New Hope'
-    end
+
+describe '5. Validar o nome correto de um determinado episódio de filme', :teste_5 do
+  it 'get' do
+    response = SWAPI.get('/api/films/1/')
+
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+    puts "\nresponse body: #{response.parsed_response}"
+
+    expect(response.code).to eq 200
+    expect(response.parsed_response['title']).to eq 'A New Hope'
+  end
 end
 
-describe '6. Validar o ID do episódio e o tipo do dado está correto', :teste_6 do
-    it 'get' do
-        response = SWAPI.get('/api/films/1')
-                
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers}"
-        puts "\nresponse body: #{response.parsed_response}"
-        
-        expect(response.parsed_response['episode_id']).to eq 4
-    end
+
+describe '6. Validar o ID do episódio e se o tipo do dado está correto', :teste_6 do
+  it 'get' do
+    response = SWAPI.get('/api/films/1/')
+
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+    puts "\nresponse body: #{response.parsed_response}"
+
+    episode_id = response.parsed_response['episode_id']
+
+    expect(response.code).to eq 200
+    expect(episode_id).to eq 4
+    expect(episode_id).to be_a(Integer)
+  end
 end
 
-describe '7. Validar o formato de data válida (padrão americano) e validar se a data não é padrão Brasil', :teste_7 do
-    it 'get' do
-        response = SWAPI.get('/api/films/1')
-                
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers}"
-        puts "\nresponse body: #{response.parsed_response}"
-        
-        expect(response.parsed_response['release_date']).to eq "1977-05-25" #Formato Americano
-        expect(response.parsed_response['release_date']).not_to eq "25/05/1977" #Formato Brasil
-    end
+
+describe '7. Validar o formato de data e garantir que não está no padrão Brasil', :teste_7 do
+  it 'get' do
+    response = SWAPI.get('/api/films/1/')
+
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+    puts "\nresponse body: #{response.parsed_response}"
+
+    release_date = response.parsed_response['release_date']
+
+    expect(response.code).to eq 200
+    expect(release_date).to eq '1977-05-25'
+    expect(release_date).to match(/^\d{4}-\d{2}-\d{2}$/)
+    expect(release_date).not_to eq '25/05/1977'
+  end
 end
 
-describe '8. Validar o peso e altura do “people” C-3PO e validar pelo menos um filme que ele tenha participado', :teste_8 do
-    it 'get' do
-        response = SWAPI.get('/api/people/?search=C-3PO')
-                
-        puts "\nresponse code: #{response.code}"
-        puts "\nresponse message: #{response.message}"
-        puts "\nresponse headers: #{response.headers}"
-        puts "\nresponse body: #{response.parsed_response['results']}"
 
-        dados = response.parsed_response['results']
+describe '8. Validar o peso e altura do people C-3PO e validar pelo menos um filme que ele tenha participado', :teste_8 do
+  it 'get' do
+    response = SWAPI.get('/api/people/?search=C-3PO')
 
-        dados.each do |item|
-            
-            puts "\nAltura: #{item["name"]}"
-            puts "\nPeso: #{item["mass"]}"
-            puts "\nFilmes: #{item["films"]}"
-            # etc
+    puts "\nresponse code: #{response.code}"
+    puts "\nresponse message: #{response.message}"
+    puts "\nresponse headers: #{response.headers}"
+    puts "\nresponse body: #{response.parsed_response['results']}"
 
-            expect(item["mass"]).to eq "75" #Valida se o valor do peso é 75
-            expect(item["films"]).to include "http://swapi.dev/api/films/1/" #Filme que pelo menos tem participado
-            expect(item["films"][0]).to eq "http://swapi.dev/api/films/1/"
+    dados = response.parsed_response['results']
 
-        end
-    end
+    expect(response.code).to eq 200
+    expect(dados).not_to be_empty
+
+    c3po = dados.first
+
+    puts "\nNome: #{c3po['name']}"
+    puts "\nAltura: #{c3po['height']}"
+    puts "\nPeso: #{c3po['mass']}"
+    puts "\nFilmes: #{c3po['films']}"
+
+    expect(c3po['name']).to eq 'C-3PO'
+    expect(c3po['height']).to eq '167'
+    expect(c3po['mass']).to eq '75'
+    expect(c3po['films']).not_to be_empty
+    expect(c3po['films']).to include('https://swapi.dev/api/films/1/')
+  end
 end
